d1d0e230c2102757e041aa94d24b586975c341a7
Alexandre <alexandre.delsol@hotmail.fr>
2026-09-25 17:29:34 +0200

    style: refine thank you card

--- src/components/ThankYouCard.vue
+++ src/components/ThankYouCard.vue
@@ -5,19 +5,45 @@ defineProps<{
 </script>
 
 <template>
-  <img src="../assets/illustration-thank-you.svg" alt="">
-    <p>You selected {{ selectedRating }} out of 5</p>
-    <h1>Thank you!</h1>
-    <p>We appreciate you taking the time to give a rating. If you ever need more support,
-      don’t hesitate to get in touch!</p>
+    <div class="thank-you-card">
+        <img class="thank-you-image" src="../assets/illustration-thank-you.svg" alt="">
+
+        <p class="rating-result">
+            You selected {{ selectedRating }} out of 5
+        </p>
+
+        <h1>Thank you!</h1>
+
+        <p class="description">
+            We appreciate you taking the time to give a rating. If you ever need more support,
+            don’t hesitate to get in touch!
+        </p>
+    </div>
 </template>
 
 <style scoped>
+
+.thank-you-card{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    gap: 20px;
+}
+
+.rating-result {
+    color: var(--color-orange);
+    background-color: var(--color-gray-900);
+    padding: 8px 16px;
+    border-radius: 20px;
+}
+
 h1 {
     color: var(--color-white)
 }
 
-p {
+.description {
     color: var(--color-gray-500);
+    font-size: 15px;
 }
 </style>
